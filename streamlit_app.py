8f07ff75a1191be30fe75bba353dbc5b85f2fa5b
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-04 09:24:08 +0530

    gender counts

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,19 +11,6 @@ st.info('Salary for various IT Positions - configure using custom inputs that su
 df = pd.read_csv('employee.csv')
 df
 
-male_count = df[df['Gender'] == 'M'].shape[0]
-female_count = df[df['Gender'] == 'F'].shape[0]
-
-male_count
-female_count
-
 # Count the number of males and females
 gender_counts = df['Gender'].value_counts()
-
-# Create a bar chart
-plt.bar(gender_counts.index, gender_counts.values)
-plt.xlabel("Gender")
-plt.ylabel("Count")
-plt.title("Number of Males and Females in the Dataset")
-bar=plt
-bar
+gender_counts
