1c64f69250ec88667e0dcd42a9518215135fe1e5
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-04 09:54:34 +0530

    removed bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,4 @@ option = st.selectbox(
 st.write('you have selected: ', option)
 
 option_count = df[option].value_counts()
-st.bar_chart(option_count, color=["#fd0", "#f0f", "#04f"]')
+st.bar_chart(option_count, color=["#fd0", "#f0f", "#04f"])
