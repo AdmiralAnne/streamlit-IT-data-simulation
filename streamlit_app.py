21cc4712fee8ac4141c0df958e676ec493c32c7f
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-04 10:09:23 +0530

    removed error code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
 
 st.title('📈 IT Data Simulation')
 
@@ -24,18 +22,7 @@ option_count = df[option].value_counts()
 st.bar_chart(option_count, color='count')
 
 # new code:
-# Function to generate a color palette
-def generate_colors(n_colors):
-    colors = plt.cm.get_cmap('tab20')(np.linspace(0, 1, n_colors))
-    return [mcolors.to_hex(color) for color in colors]
-
-st.write('you have selected: ', option)
 
 # Count occurrences
 option_count = df[option].value_counts()
-
-# Generate colors based on the number of unique values
-colors = generate_colors(len(option_count))
-
-# Create the bar chart with custom colors
-st.bar_chart(option_count, color=colors)
+option_count
